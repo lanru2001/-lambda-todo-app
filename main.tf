d8414a5681e1d7f3e5e4fb483b4bd6f6c6951dea
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2025-05-17 18:28:45 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,205 @@
+resource "aws_dynamodb_table" "todos" {
+  name         = "Todos"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "userId"
+  range_key    = "todoId"
+
+  attribute {
+    name = "userId"
+    type = "S"
+  }
+
+  attribute {
+    name = "todoId"
+    type = "S"
+  }
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda_exec_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_policy" "dynamodb_access" {
+  name        = "DynamoDBAccessPolicy"
+  description = "Policy for Lambda to access DynamoDB"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:UpdateItem",
+          "dynamodb:DeleteItem",
+          "dynamodb:Scan",
+          "dynamodb:Query"
+        ]
+        Effect   = "Allow"
+        Resource = aws_dynamodb_table.todos.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_dynamodb" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = aws_iam_policy.dynamodb_access.arn
+}
+
+resource "aws_lambda_function" "todo_api" {
+  filename      = "todo_api.zip"
+  function_name = "TodoAPI"
+  role          = aws_iam_role.lambda_exec.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.9"
+
+  source_code_hash = filebase64sha256("todo_api.zip")
+
+  environment {
+    variables = {
+      TODOS_TABLE = aws_dynamodb_table.todos.name
+    }
+  }
+}
+
+resource "aws_api_gateway_rest_api" "todo_api" {
+  name        = "TodoAPI"
+  description = "API for Todo Application"
+}
+
+resource "aws_api_gateway_resource" "todos" {
+  rest_api_id = aws_api_gateway_rest_api.todo_api.id
+  parent_id   = aws_api_gateway_rest_api.todo_api.root_resource_id
+  path_part   = "todos"
+}
+
+resource "aws_api_gateway_resource" "todo" {
+  rest_api_id = aws_api_gateway_rest_api.todo_api.id
+  parent_id   = aws_api_gateway_resource.todos.id
+  path_part   = "{todoId}"
+}
+
+resource "aws_api_gateway_method" "get_todos" {
+  rest_api_id   = aws_api_gateway_rest_api.todo_api.id
+  resource_id   = aws_api_gateway_resource.todos.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method" "post_todo" {
+  rest_api_id   = aws_api_gateway_rest_api.todo_api.id
+  resource_id   = aws_api_gateway_resource.todos.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method" "get_todo" {
+  rest_api_id   = aws_api_gateway_rest_api.todo_api.id
+  resource_id   = aws_api_gateway_resource.todo.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method" "put_todo" {
+  rest_api_id   = aws_api_gateway_rest_api.todo_api.id
+  resource_id   = aws_api_gateway_resource.todo.id
+  http_method   = "PUT"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method" "delete_todo" {
+  rest_api_id   = aws_api_gateway_rest_api.todo_api.id
+  resource_id   = aws_api_gateway_resource.todo.id
+  http_method   = "DELETE"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "get_todos" {
+  rest_api_id             = aws_api_gateway_rest_api.todo_api.id
+  resource_id             = aws_api_gateway_resource.todos.id
+  http_method             = aws_api_gateway_method.get_todos.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.todo_api.invoke_arn
+}
+
+resource "aws_api_gateway_integration" "post_todo" {
+  rest_api_id             = aws_api_gateway_rest_api.todo_api.id
+  resource_id             = aws_api_gateway_resource.todos.id
+  http_method             = aws_api_gateway_method.post_todo.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.todo_api.invoke_arn
+}
+
+resource "aws_api_gateway_integration" "get_todo" {
+  rest_api_id             = aws_api_gateway_rest_api.todo_api.id
+  resource_id             = aws_api_gateway_resource.todo.id
+  http_method             = aws_api_gateway_method.get_todo.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.todo_api.invoke_arn
+}
+
+resource "aws_api_gateway_integration" "put_todo" {
+  rest_api_id             = aws_api_gateway_rest_api.todo_api.id
+  resource_id             = aws_api_gateway_resource.todo.id
+  http_method             = aws_api_gateway_method.put_todo.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.todo_api.invoke_arn
+}
+
+resource "aws_api_gateway_integration" "delete_todo" {
+  rest_api_id             = aws_api_gateway_rest_api.todo_api.id
+  resource_id             = aws_api_gateway_resource.todo.id
+  http_method             = aws_api_gateway_method.delete_todo.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.todo_api.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "todo_api" {
+  depends_on = [
+    aws_api_gateway_integration.get_todos,
+    aws_api_gateway_integration.post_todo,
+    aws_api_gateway_integration.get_todo,
+    aws_api_gateway_integration.put_todo,
+    aws_api_gateway_integration.delete_todo,
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.todo_api.id
+  stage_name  = "prod"
+}
+
+resource "aws_lambda_permission" "api_gateway" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.todo_api.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.todo_api.execution_arn}/*/*"
+}
+
+output "api_url" {
+  value = aws_api_gateway_deployment.todo_api.invoke_url
+}
